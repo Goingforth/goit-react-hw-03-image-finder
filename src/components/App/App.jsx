b3061cdfde0cfc308fbe93e1008eebdada2be082
Oleksandr Siukov <as-service2008@ukr.net>
2023-05-09 23:02:45 +0300

    modal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,37 @@
-export const App = () => {
-  return <div>React homework template</div>;
-};
+import React from 'react';
+import Modal from 'components/Modal/Modal';
+// import { nanoid } from 'nanoid';
+// import ContactForm from 'components/ContactForm/ContactForm';
+// import { Container } from './Container.styled';
+// import Filter from 'components/Filter/Filter';
+// import ContactList from 'components/ContactList/ContactList';
+class App extends React.Component {
+  state = {
+    showModal: false,
+  };
+  // componentDidMount() {}
+
+  // componentDidUpdate(prevProps, prevState) {}
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div>
+        <button type="button" onClick={this.toggleModal}>
+          Open modal
+        </button>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <p>Loren</p>
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
